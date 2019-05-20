44e172c2ca77b48f03f59294624ae31431827aab
oldfish007 <47681668+oldfish007@users.noreply.github.com>
2019-05-20 17:53:26 +0800

    影片模块 首页接口

--- guns-gateway/src/main/java/com/stylefeng/guns/rest/modular/film/FilmController.java
+++ guns-gateway/src/main/java/com/stylefeng/guns/rest/modular/film/FilmController.java
@@ -1,5 +1,8 @@
 package com.stylefeng.guns.rest.modular.film;
 
+import com.alibaba.dubbo.config.annotation.Reference;
+import com.stylefeng.guns.api.film.FilmServiceAPI;
+import com.stylefeng.guns.rest.modular.film.vo.FilmIndexVO;
 import com.stylefeng.guns.rest.modular.vo.ResponseVO;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -16,25 +19,29 @@ public class FilmController {
 
 
     //获取首页信息接口
-
+     @Reference(interfaceClass = FilmServiceAPI.class)
+     private  FilmServiceAPI filmServiceAPI;
     /**
      * API 【聚合】
      *
      * @return
      */
     @RequestMapping(value="getIndex",method = RequestMethod.GET)
-    public ResponseVO getIndex(){
+    public ResponseVO<FilmIndexVO> getIndex(){
+
+        FilmIndexVO filmIndexVO = new FilmIndexVO();
         //获取banner信息
-
+        filmIndexVO.setBanners(filmServiceAPI.getBanners());
         //获取热映电影
-
+        filmIndexVO.setHotFilms(filmServiceAPI.getHotFilms(true,8));
         //即将上映电影
-
+        filmIndexVO.setSoonFilms(filmServiceAPI.getSoonFilms(true,8));
         //票房排行榜
-
+        filmIndexVO.setBoxRanking(filmServiceAPI.getBoxRanking());
         //受欢迎人气榜
-
+        filmIndexVO.setExpectRanking(filmServiceAPI.getExpertRanking());
         //top100
-        return null;
+        filmIndexVO.setTop100(filmServiceAPI.getTop());
+        return ResponseVO.success(filmIndexVO);
     }
 }
